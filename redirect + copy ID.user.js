963336ad46273c7a7233611b38b5f4716c0dc963
Min <tuongminh1024@gmail.com>
2026-01-21 13:46:04 +0700

    Update and rename Redirect Skipper.user.js to redirect + copy ID.user.js

---
+++ redirect + copy ID.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Redirect & Auto Copy ID
+// @namespace    
+// @version      1.2
+// @description  Redirect và tự động copy mã truyện vào Clipboard
+// @author       Minty
+// @match        *://*/redirect?*
+// @grant        GM_setClipboard
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const targetUrlEncoded = urlParams.get('u');
+
+    if (targetUrlEncoded) {
+        // 1. Giải mã URL
+        const decodedUrl = decodeURIComponent(targetUrlEncoded);
+
+        // 2. Tách mã truyện (ID)
+        // Logic: Lấy phần cuối cùng của đường dẫn, loại bỏ các tham số query sau dấu ?
+        try {
+            const urlObj = new URL(decodedUrl);
+            const pathSegments = urlObj.pathname.split('/').filter(s => s.length > 0);
+            const storyId = pathSegments[pathSegments.length - 1];
+
+            if (storyId) {
+                // 3. Copy mã truyện vào Clipboard
+                GM_setClipboard(storyId);
+                console.log("Đã copy mã truyện: " + storyId);
+            }
+        } catch (e) {
+            console.error("Không thể tách mã truyện", e);
+        }
+
+        // 4. Chuyển hướng
+        window.location.replace(decodedUrl);
+    }
+})();
